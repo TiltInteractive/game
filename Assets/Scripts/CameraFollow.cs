f73d9ccc751937faec809229016308251076a953
Данила Золотов <zolotov.dk@students.dvfu.ru>
2023-03-25 10:52:36 +1000

    begin

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public bool smooth = false;
+    public float Speed;
+    public GameObject Player;
+    public GameObject Camera;
+
+    void FixedUpdate()
+    {
+        if (smooth) {
+            Vector3 target = new Vector3()
+            {
+                x = Player.transform.position.x,
+                y = Player.transform.position.y,
+                z = Player.transform.position.z - 10,
+            };
+            Camera.transform.position = Vector3.Lerp(Camera.transform.position, target, Speed * Time.fixedDeltaTime);
+        }
+    }
+
+
+    public Transform lookAt;
+    public float boundX = 0.15f;
+    public float boundY =  0.05f;
+
+    private void LateUpdate()
+    {
+        if(!smooth) {
+        Vector3 delta = Vector3.zero;
+        // X axis
+        float deltaX = lookAt.position.x - transform.position.x;
+        if(deltaX > boundX || deltaX < -boundX)
+        {
+            if(transform.position.x < lookAt.position.x)
+            {
+                delta.x = deltaX - boundX;
+
+            } else {
+                delta.x = deltaX + boundX;
+            }
+        }
+        // Y axis
+        float deltaY = lookAt.position.y - transform.position.y;
+        if(deltaY > boundY || deltaY < -boundY) {
+            if(transform.position.y < lookAt.position.y)
+            {
+                delta.y = deltaY - boundY;
+
+            }
+            else {
+                delta.y = deltaY + boundY;
+            }
+        }
+
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
+        
+    }
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    private Vector2 direction;
+    private Rigidbody2D rb;
+
+
+    // private BoxCollider2D boxCollider;
+    // private Vector3 moveDelta;
+    // private float WalkSpeed = 0f;
+    // private float MaxSpeed = 2f;
+    // private RaycastHit2D hit;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+      //  boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update() {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.y = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(direction);
+        rb.MovePosition(rb.position + direction * speed  * Time.fixedDeltaTime);
+
+
+        // float x = Input.GetAxisRaw("Horizontal");
+        // float y = Input.GetAxisRaw("Vertical");
+
+
+        // Debug.Log(hit.collider);
+
+        // // Reset moveDelta
+        // moveDelta = new Vector3(x * WalkSpeed,y * WalkSpeed,0);
+
+        // // Swap sp direct
+        if (direction.x < 0) {
+            transform.localScale = Vector3.one;
+        }
+        else if (direction.x > 0) {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Creature", "Block"));
+        
+        // if (hit.collider == null)
+        // {
+        //     transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        // }
+
+
+        // hit = Physics2D.BoxCast(transform.position,boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Creature", "Block"));
+        
+        // if (hit.collider == null)
+        // {
+        //     transform.Translate(moveDelta.x * Time.deltaTime, 0 , 0);
+        // }
+
+
+        // // dvizhenie
+
+
+        // if (x != 0 || y != 0)
+        // {
+        //     if (WalkSpeed < MaxSpeed) { WalkSpeed = WalkSpeed + 0.2f * MaxSpeed; }
+
+        // } else
+        // {
+        //     if (WalkSpeed > 0f) { WalkSpeed = 0; }
+                
+        // }
+    }
+}
