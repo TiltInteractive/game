a1f067c8424918049ebb20cb7a3e8927777f4e43
Данила Золотов <zolotov.dk@students.dvfu.ru>
2023-05-20 10:05:56 +1000

    фикс багов, зона торговца, диалоговые окна

--- Assets/DialogManager.cs
+++ Assets/DialogManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 public class DialogManager : MonoBehaviour
 {
     public GameObject Dialog;
+    public GameObject shopMenu;
     public TMP_Text Text;
     public string[] message;
     public bool dialogStat = false;
@@ -12,7 +13,7 @@ public class DialogManager : MonoBehaviour
     void Start()
     {
         message = new string[10];
-        message[0] = "asd";
+        message[0] = "Аптечка";
         message[1] = "asdt";
         Dialog.SetActive(false);
         Debug.Log("Asdasd");
@@ -23,7 +24,7 @@ public class DialogManager : MonoBehaviour
         if (collision.tag == "Player")
         {
             Dialog.SetActive(true);
-            Text.text = message[0];
+
             dialogStat = true;
         }
     }
@@ -34,11 +35,42 @@ public class DialogManager : MonoBehaviour
         {
             Dialog.SetActive(false);
             dialogStat = false;
+            shopMenu.SetActive(false);
+    
+        }
+    }
+
+    void movementToggle(bool f)
+    {
+        GameObject temp = GameObject.Find("Player");
+        Player other = temp.GetComponent<Player>();
+        if (f)
+        {
+            other.enabled = true;
+        }
+        else
+        {
+            other.enabled = false;
         }
     }
     // Update is called once per frame
     void Update()
     {
-        
+        if (dialogStat)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Dialog.SetActive(false);
+                shopMenu.SetActive(dialogStat);
+                movementToggle(false);
+            }
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Dialog.SetActive(true);
+                shopMenu.SetActive(false);
+                movementToggle(true);
+            }
+
+        }
     }
 }
