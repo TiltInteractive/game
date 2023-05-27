f92caa8f84e64e5ee308a45a16c425eef7d128c2
Данила Золотов <zolotov.dk@students.dvfu.ru>
2023-05-27 12:55:27 +1000

    piz.ez

--- Assets/AgentMovement.cs
+++ Assets/AgentMovement.cs
@@ -7,13 +7,16 @@ public class AgentMovement : MonoBehaviour
 {
     private Vector3 target;
     NavMeshAgent agent;
-    public GameObject player;
+    /*    public GameObject player;*/
     // Start is called before the first frame update
-    private void Awake()
+    void Start()
     {
+        Debug.Log("asd");
+
         agent = GetComponent<NavMeshAgent>();
         agent.updatePosition = false;
         agent.updateRotation = false;
+        agent.Warp(new Vector3(0, 0 ,transform.position.z));
     }
 
     private void Update()
@@ -23,9 +26,8 @@ public class AgentMovement : MonoBehaviour
 
     private void SetTargetPosition()
     {
-        Vector2 tpos;
-        tpos.x = player.GetComponent<Rigidbody2D>().position.x;
-        tpos.y = player.GetComponent<Rigidbody2D>().position.y;
-        agent.SetDestination(new Vector3(tpos.x, tpos.y, transform.position.z));
+/*        target.x = player.GetComponent<Rigidbody2D>().position.x;
+        target.y = player.GetComponent<Rigidbody2D>().position.y;*/
+        agent.SetDestination(new Vector3(10, 10, transform.position.z));
     }
 }
