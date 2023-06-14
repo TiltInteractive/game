9353b5780cca5ce89f186df75d3909ea9ae782c0
Богдан U <bg26092003@gmail.com>
2023-06-14 11:49:54 +1000

    final 0.6

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using static UnityEditor.PlayerSettings;
+/*using static UnityEditor.PlayerSettings;*/
 
 public class Health : MonoBehaviour
 {
